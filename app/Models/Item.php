d001f30628148812096f95fbb635df0d71290fe2
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2024-01-31 16:12:59 +0630

    db testing

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+    //model ko item lo name py yin migration mhr table ko items lo py htr yin model nk table nk auto chate p tr ma hk woo so yin dot d mhr protected $table='tbl_name' lo yay p chate py ya ml 
 }
