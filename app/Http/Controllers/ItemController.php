59d44363b34762d059fa907a0c4477504cf41b44
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2024-01-31 16:23:31 +0630

    test storing data

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,18 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('inventory.create');
     }
-    public function index(){
+    public function index()
+    {
         return view('inventory.index');
     }
-    public function store(Request $request){
-        dd($request);
-        return $request;
+    public function store(Request $request)
+    {
+        $item = new Item();
+        $item->name = $request->name;
+        $item->price = $request->price;
+        $item->stock = $request->stock;
+        $item->save();
+        //object instance ma sout chin yin Item modal class htl ka create static method ko d lo sout lo ya $items=Item::create(['name'=>'apple',price=>500])
+        return $item;
     }
 }
