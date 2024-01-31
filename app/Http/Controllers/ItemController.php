f890206a4dd358533dfbd175c56facfa3ee8ac1f
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2024-01-31 17:03:42 +0630

    all item now showing

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -13,7 +13,10 @@ class ItemController extends Controller
     }
     public function index()
     {
-        return view('inventory.index');
+        //d mhr ll $items= new Item() so p instatnce sout p mha $items->all() so p use lo ll ya dl
+        return view('inventory.index', [
+            'items' => Item::all()
+        ]);
     }
     public function store(Request $request)
     {
@@ -22,7 +25,7 @@ class ItemController extends Controller
         $item->price = $request->price;
         $item->stock = $request->stock;
         $item->save();
-        //object instance ma sout chin yin Item modal class htl ka create static method ko d lo sout lo ya $items=Item::create(['name'=>'apple',price=>500])
-        return $item;
+        //object instance ma sout chin yin Item modal class htl ka create static method ko d lo sout lo ya $items=Item::create(['name'=>'apple',price=>500])but d nee ko tone ml so yin modal htl mhr protected $fillable=['name','price','stock']; so p twr yay py ya ml//create() asar insert() ll tone lo ya dl but  insert ka eloquent ORM nk tone dr ma hk pl query builder nk tone dr fik twr p // insert ko ton yin ka insert ka fb ko takhr pl khine dl data store pho takhu pl create ka kya dot db ko data store dl id u dl id nk db htl ka data ko pyn htote dl 3 khr lout khine dl inser ka created at updated at dg ma htl py woo
+        return redirect()->route('item.index');
     }
 }
--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -4,7 +4,34 @@ Item list
 @endsection
  @section('content')
 <h1>Item list </h1>
+<table class=" table table-borderless">
+    <thead>
+        <tr>
+            <td>#</td>
+            <td>Name</td>
+            <td>Price</td>
+            <td>Stock</td>
 
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($items as $item)
+        <tr>
+            <td>{{$item->id}}</td>
+            <td>{{$item->name}}</td>
+            <td>{{$item->price}}</td>
+            <td>{{$item->stock}}</td>
+        </tr>
+        @empty
+        <tr>
+            <td colspan="4" class=" text-center  mt-5">
+                There is no record <br>
+                <a class=" btn btn-sm btn-primary" href="{{route('item.create')}}">Create Item</a>
+            </td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
 
  @endsection
 
